f96e4fcb2aeddff0957d7f334bfb27f690b62001
Giuseppe R <hello@russogiuseppe.net>
2021-01-25 17:51:47 +0100

    fixtures

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -4,13 +4,22 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Products;
 
 class ProductsFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+     
+        for($i = 1; $i <= 10; $i++){
+            $products = new Product();
+            $products->setTitle("Titre du produit n°$i")
+                     ->setContent("<p>Description du produit N°$i</p>")
+                     ->setImage("http://olacehold.it/350x150");
+
+            $manager->persist($products);
+
+        }
 
         $manager->flush();
     }
