29304a39f64486321ae92aa02aa7aadb3130cf0b
Giuseppe R <hello@russogiuseppe.net>
2021-02-09 17:27:21 +0100

    correction function add et css

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Products;
 use App\Repository\ProductsRepository;
